e5f64f196b7c68c0123acf4375885212c4de860d
callumstephenson <callumstephenson03@gmail.com>
2022-04-20 16:32:08 +0100

    changed set.py random answer generator

--- set.py
+++ set.py
@@ -15,7 +15,7 @@ class set():
         for each in self.card_list:
             weight_list.append(each.weight)
         weight_list = tuple(weight_list)
-        return rand.choices(self.card_list,weights=weight_list, k=1)[0]
+        return rand.choices(self.card_list,weights=weight_list)[0]
     
     def question_builder(self,answer_num):
         'builds a question of "answer_num" possibilities, returns in format question,correct ans, answer possibility list'
@@ -24,9 +24,8 @@ class set():
         temp_cardlist.remove(selected_card)
         answer_list = []
         for i in range(answer_num-1):
-            tempcard = rand.choices(temp_cardlist, k=1)[0]
-            answer_list.append(tempcard.answer)
-            temp_cardlist.remove(tempcard)
+            rand.shuffle(temp_cardlist)
+            answer_list.append(temp_cardlist.pop().answer)
         answer_list.append(selected_card.answer)
         rand.shuffle(answer_list)
         return selected_card.question, selected_card.answer, answer_list, selected_card
